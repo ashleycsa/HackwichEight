cb7d4c23beca65947d290b21ae1fd95834253e19
Ashley Aurellano <ashley.aurellano@hotmail.com>
2020-03-10 10:42:36 -1000

    3/10 - Completed Part 1 & 2 of Hackwich #5

--- HackwichEight/ViewController.swift
+++ HackwichEight/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var slider: UISlider!
+    //create a variable that can hold the value of the slider
+    var currentValue: Int = 0
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func guessNumberPressed(_ sender: Any)
+    {
+        //5. new variable message that displays a message that includes currentValue
+        let message = "The value is: \(currentValue)"
+        
+        //1. create alert view
+        let alert = UIAlertController(title: "Hello World!", message: message, preferredStyle: .alert)
+        
+        //2. button that user taps to dismiss view controller
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+             
+        //3. add the button to alertview
+        alert.addAction(action)
+             
+        //4. present alertview on the screen
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func sliderHasMoved(_ sender: Any)
+    {
+        print("The value of the slider is:\(slider.value)")
+        //slider value's data type is Float, while currentValue data type is Int. this convertsthe Float value to Int value
+        currentValue = lroundf(slider.value)
+    }
 }
 
