d5d980e15f5f1bbd24c654b72fbebbba590041f5
Ashley Aurellano <ashley.aurellano@hotmail.com>
2020-03-13 13:23:32 -1000

    Completed Part 3 and Problem Sets 1 and 2

--- HackwichEight/ViewController.swift
+++ HackwichEight/ViewController.swift
@@ -12,33 +12,50 @@ class ViewController: UIViewController {
 
     
     @IBOutlet weak var slider: UISlider!
+    
+    @IBOutlet weak var targetLabel: UILabel!
+    
     //create a variable that can hold the value of the slider
     var currentValue: Int = 0
     
+    //Part 3&4, part 2a
+    var targetValue: Int = 0
     
-    
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //Part 3&4, part 1 - set currentValue to the initial value of the slider
+        currentValue = lroundf(slider.value)
+        targetValue = Int.random(in: 0...100)
+        
+        //call startNewRound
+        startNewRound()
     }
 
-
-    @IBAction func guessNumberPressed(_ sender: Any)
+  
+    @IBAction func myGuessButtonPressed(_ sender: Any)
     {
         //5. new variable message that displays a message that includes currentValue
-        let message = "The value is: \(currentValue)"
-        
-        //1. create alert view
-        let alert = UIAlertController(title: "Hello World!", message: message, preferredStyle: .alert)
-        
-        //2. button that user taps to dismiss view controller
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
-             
-        //3. add the button to alertview
-        alert.addAction(action)
-             
-        //4. present alertview on the screen
-        present(alert, animated: true, completion: nil)
+          //Part 3&4, Part 2 #5 - Display currentValue and targetValue in the alert view’s message
+           let message = "The current slider value is: \(currentValue)" + "\nThe target value is: \(targetValue)"
+           
+           //1. create alert view
+           let alert = UIAlertController(title: "Hello World!", message: message, preferredStyle: .alert)
+           
+           //2. button that user taps to dismiss view controller
+           //Part 3&4, Problem set 2
+           let action = UIAlertAction(title: "New Round", style: .default, handler: nil)
+                
+           //3. add the button to alertview
+           alert.addAction(action)
+                
+           //4. present alertview on the screen
+           present(alert, animated: true, completion: nil)
+           
+           //Part 3&4, part 3 #3-5
+           startNewRound()
     }
     
     
@@ -47,6 +64,23 @@ class ViewController: UIViewController {
         print("The value of the slider is:\(slider.value)")
         //slider value's data type is Float, while currentValue data type is Int. this convertsthe Float value to Int value
         currentValue = lroundf(slider.value)
+        
+        //Part 3&4, part 3 - create new round function
+      }
+    
+    func startNewRound ()
+    {
+        targetValue = Int.random(in: 0...100)
+        currentValue = lroundf(slider.value)
+        
+        //Part 3&4, problem set 1, part 3
+        updateTargetLabel()
     }
-}
+    
+    //Part 3&4, problem set 1, part 2
+    func updateTargetLabel ()
+    {
+        self.targetLabel.text = "\(targetValue)"
+    }
 
+}
